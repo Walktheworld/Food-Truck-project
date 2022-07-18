8d7749172edaf2e37d70a4a11f996638e24f832f
Joe <Walktheworld@me.com>
2022-07-18 10:51:55 -0600

    connected custom route to profile

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ApplicationRecord
     belongs_to :user
     # belongs_to :creator, class_name: "User"
     has_many :clients, through: :reviews, source: :user
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
     has_many :reviewers, through: :reviews, source: :reviewer
 
 end
